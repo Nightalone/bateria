33e85d3419d5d48d75f33692b1a87ca51253ea20
Dyego Castro <dcastrocerqueira33@gmail.com>
2023-04-13 23:36:36 -0300

    Projeto atualizado

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ let drumNumber = document.querySelectorAll('.drum').length
 
 for(let i = 0; i < drumNumber; i++){
     
-    document.querySelectorAll("button")[i].addEventListener('click', () => { alert("Hello guys") });
+    document.querySelectorAll("button")[i].addEventListener('click', () => { 
+        let audio = new Audio("./sounds/tom-1.mp3");
+        audio.play(); 
+    });
 }
 
 
